730b596749204d39dcd046144689e7ff3cddac78
joaomlg <joaomarcos1999jm@gmail.com>
2021-08-30 21:57:40 -0300

    CreateUsers migration

---
+++ src/database/migrations/1630370040211-CreateUsers.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1630370040211 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {name: 'id', type: 'uuid', isPrimary: true},
+        {name: 'name', type: 'varchar'},
+        {name: 'email', type: 'varchar'},
+        {name: 'admin', type: 'boolean', default: false},
+        {name: 'created_at', type: 'boolean', default: 'now()'},
+        {name: 'updated_at', type: 'boolean', default: 'now()'}
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}

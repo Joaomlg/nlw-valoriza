a89d2210186d4c7134a9280e08a9b588d25c2bbd
joaomlg <joaomarcos1999jm@gmail.com>
2021-08-31 19:41:32 -0300

    feature: middleware to treat throws and return error

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
 import { router } from "./routes";
 
@@ -9,6 +10,20 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 app.listen(port, () => console.log("Server is running on localhost:3000"));

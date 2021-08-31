4497e2039a5f9a74507ec920818e040ca667dde4
joaomlg <joaomarcos1999jm@gmail.com>
2021-08-30 23:22:26 -0300

    fix: await service create user before return

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,9 @@ import { router } from "./routes";
 import "./database";
 
 const app = express();
+const port = 3000;
 
 app.use(express.json());
-
 app.use(router);
-
-const port = 3000;
 
 app.listen(port, () => console.log("Server is running on localhost:3000"));

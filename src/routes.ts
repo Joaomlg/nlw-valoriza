8b246957be8ab849ef99393e6d293148e6d2e900
joaomlg <joaomarcos1999jm@gmail.com>
2021-09-01 20:49:02 -0300

    feature: authenticate implemented

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
@@ -10,5 +11,8 @@ router.post("/users", createUserController.handle);
 
 const createTagController = new CreateTagController();
 router.post("/tags", ensureAdmin, createTagController.handle);
+
+const authenticateUserController = new AuthenticateUserController()
+router.post("/login", authenticateUserController.handle);
 
 export { router };
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,38 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password } : IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UserRepositories);
+
+    const user = await usersRepositories.findOne({ email });
+
+    if (!user) {
+      throw new Error('Email/password incorrect')
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email/password incorrect')
+    }
+
+    const token = sign({
+      email: user.email
+    }, "7fb391545b7ca20feae5ab0b07a4fc6b", {
+      subject: user.id,
+      expiresIn: "1d"
+    })
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };

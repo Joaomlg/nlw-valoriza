e6216c3d55d9b9a4b1887df141976e67c44f91ed
joaomlg <joaomarcos1999jm@gmail.com>
2021-09-08 20:19:16 -0300

    Fix: ensure admin middleware check user by token

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,13 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-  const admin = true;  // Temporary
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+  const { user_id } = request;
+  
+  const usersRepositories = getCustomRepository(UserRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
 
   if (admin) {
     return next();
